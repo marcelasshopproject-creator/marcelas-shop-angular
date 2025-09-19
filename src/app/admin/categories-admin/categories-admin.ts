a29c53d8a39a4658a9fdcad06c97b3686c684286
Alex Coronell <alexcoronell@gmail.com>
2025-09-19 12:18:58 -0500

    Categories table basic layout

--- src/app/admin/categories-admin/categories-admin.ts
+++ src/app/admin/categories-admin/categories-admin.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-categories-admin',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './categories-admin.html',
   styleUrl: './categories-admin.scss'
 })
