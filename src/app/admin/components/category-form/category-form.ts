767bde6578b200a56dce79d5e8670235ea51f7d9
Alex Coronell <alexcoronell@gmail.com>
2025-09-19 14:02:00 -0500

    CreateCategory completed

--- src/app/admin/components/category-form/category-form.ts
+++ src/app/admin/components/category-form/category-form.ts
@@ -1,11 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+/* Components */
+import { Loading } from '../../../shared/ui/loading/loading';
+
+/* Services */
+import { CategoryData } from '../../../core/services/category-data';
+
+/* Interfaces */
+import { Category } from '../../../core/interfaces/category';
+
+/* DTO's */
+import { CreateCategoryDto } from '../../../core/dtos/create-category.dto';
+import { UpdateCategoryDto } from '../../../core/dtos/update-category.dto';
+
+/* Types */
+import { RequestStatus } from '../../../core/types/request-status-type';
 
 @Component({
   selector: 'app-category-form',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink, Loading, RouterLink],
   templateUrl: './category-form.html',
-  styleUrl: './category-form.scss'
 })
 export class CategoryForm {
+  private formBuilder: FormBuilder = inject(FormBuilder);
+  private categoryService: CategoryData = inject(CategoryData);
+  requestStatus = signal<RequestStatus>('init');
+  category = signal<Category | null>(null);
 
+  form = this.formBuilder.group({
+    name: ['', [Validators.required]],
+  });
+
+  async save() {
+    this.form.markAllAsTouched();
+    if (!this.form.valid) return;
+    this.form.disable();
+    this.requestStatus.set('loading');
+
+    const dto: CreateCategoryDto = this.form.getRawValue() as CreateCategoryDto;
+
+    const { data, error } = await this.categoryService.create(dto);
+
+    if (error) {
+      this.requestStatus.set('error');
+      setTimeout(() => {
+        this.requestStatus.set('init');
+        this.form.enable();
+      }, 3000);
+      return;
+    }
+
+    if (data) {
+      this.requestStatus.set('success');
+
+      setTimeout(() => {
+        this.requestStatus.set('init');
+        this.form.reset();
+        this.form.enable();
+      }, 3000);
+    }
+  }
 }
---
+++ src/app/core/services/category-data.ts
@@ -0,0 +1,31 @@
+import { Injectable, inject, signal } from '@angular/core';
+
+import { SupabaseService } from '../../database/supabase-service';
+
+import { Category } from '../interfaces/category';
+
+import { CreateCategoryDto } from '../dtos/create-category.dto';
+import { UpdateCategoryDto } from '../dtos/update-category.dto';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoryData {
+  private supabaseService = inject(SupabaseService);
+  private supabase = this.supabaseService.supabase;
+
+  private TABLENAME = 'categories';
+
+  getAll = async () => this.supabase.from(this.TABLENAME).select('*');
+
+  get = async (id: Category['id']) =>
+    this.supabase.from(this.TABLENAME).select('*').eq('id', id).single();
+
+  create = async (dto: CreateCategoryDto) =>
+    this.supabase.from(this.TABLENAME).insert(dto).select();
+
+  update = async (id: Category['id'], dto: UpdateCategoryDto) =>
+    this.supabase.from(this.TABLENAME).update(dto).eq('id', id).select();
+
+  delete = async (id: Category['id']) => this.supabase.from(this.TABLENAME).delete().eq('id', id);
+}
