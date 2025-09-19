8404b73b83749759f6b348ad2b73ff6f0aa61878
Alex Coronell <alexcoronell@gmail.com>
2025-09-19 09:13:34 -0500

    Admin page completed

--- src/app/admin/admin-shell/admin.routes.ts
+++ src/app/admin/admin-shell/admin.routes.ts
@@ -1,5 +1,14 @@
 import { Routes } from '@angular/router';
 
 export default [
-  { path: '', loadComponent: () => import('../admin').then((m) => m.Admin) },
+  { path: '', loadComponent: () => import('../admin').then((c) => c.Admin) },
+  {
+    path: 'productos',
+    loadComponent: () => import('../products-admin/products-admin').then((c) => c.ProductsAdmin),
+  },
+  {
+    path: 'categorias',
+    loadComponent: () =>
+      import('../categories-admin/categories-admin').then((c) => c.CategoriesAdmin),
+  },
 ] as Routes;
---
+++ src/app/admin/products-admin/products-admin.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-admin',
+  imports: [],
+  templateUrl: './products-admin.html',
+  styleUrl: './products-admin.scss'
+})
+export class ProductsAdmin {
+
+}
