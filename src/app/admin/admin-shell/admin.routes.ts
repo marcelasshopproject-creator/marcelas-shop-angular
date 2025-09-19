f1f90367aec636c2278c0d4e6a63c12a306290bd
Alex Coronell <alexcoronell@gmail.com>
2025-09-19 12:44:59 -0500

    Product and Category pages for create and edit created

--- src/app/admin/admin-shell/admin.routes.ts
+++ src/app/admin/admin-shell/admin.routes.ts
@@ -4,11 +4,29 @@ export default [
   { path: '', loadComponent: () => import('../admin').then((c) => c.Admin) },
   {
     path: 'productos',
-    loadComponent: () => import('../products-admin/products-admin').then((c) => c.ProductsAdmin),
+    loadComponent: () => import('../pages/products-admin/products-admin').then((c) => c.ProductsAdmin),
+  },
+  {
+    path: 'productos/crear',
+    loadComponent: () =>
+      import('../pages/product-create/product-create').then((c) => c.ProductCreate),
+  },
+  {
+    path: 'productos/editar/:id',
+    loadComponent: () => import('../pages/product-edit/product-edit').then((c) => c.ProductEdit),
   },
   {
     path: 'categorias',
     loadComponent: () =>
-      import('../categories-admin/categories-admin').then((c) => c.CategoriesAdmin),
+      import('../pages/categories-admin/categories-admin').then((c) => c.CategoriesAdmin),
+  },
+  {
+    path: 'categorias/crear',
+    loadComponent: () =>
+      import('../pages/category-create/category-create').then((c) => c.CategoryCreate),
+  },
+  {
+    path: 'categorias/editar/:id',
+    loadComponent: () => import('../pages/category-edit/category-edit').then((c) => c.CategoryEdit),
   },
 ] as Routes;
