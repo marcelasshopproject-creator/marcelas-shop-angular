c62525ecda85241351d50ebc29749d5f90c0ee15
Alex Coronell <alexcoronell@gmail.com>
2025-09-20 18:10:25 -0500

    Load product in edit form completed

--- src/app/admin/components/product-form/product-form.ts
+++ src/app/admin/components/product-form/product-form.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, inject, signal } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { RouterLink, ActivatedRoute, Params } from '@angular/router';
 
 /* Components */
 import { Loading } from '../../../shared/ui/loading/loading';
@@ -19,6 +19,7 @@ import { CreateProductDto } from '../../../core/dtos/create-product.dto';
 import { UpdateProductDto } from '../../../core/dtos/update-product.dto';
 
 /* Types */
+import { FormMode } from '../../../core/types/form-mode-type';
 import { RequestStatus } from '../../../core/types/request-status-type';
 
 @Component({
@@ -26,23 +27,34 @@ import { RequestStatus } from '../../../core/types/request-status-type';
   imports: [ReactiveFormsModule, RouterLink, Loading],
   templateUrl: './product-form.html',
 })
-export class ProductForm implements OnInit{
-  private formBuilder: FormBuilder = inject(FormBuilder);
+export class ProductForm implements OnInit {
+  /* Services */
+  private activateRoute = inject(ActivatedRoute);
   private categoryService: CategoryData = inject(CategoryData);
+  private formBuilder: FormBuilder = inject(FormBuilder);
   private productService: ProductData = inject(ProductData);
   private uploadImageService: UploadImageService = inject(UploadImageService);
+
+  /* constants */
+  BUCKET_NAME = 'product_images';
+
+  /* Signals */
+  formMode = signal<FormMode>('create');
   requestStatus = signal<RequestStatus>('init');
   requestStatusCategories = signal<RequestStatus>('init');
+  requestStatusLoading = signal<RequestStatus>('init');
+  productId = signal<Product['id'] | null>(null);
   product = signal<Product | null>(null);
-  categories = signal<Category[]>([])
+  categories = signal<Category[]>([]);
+  errorMessage = signal<string>('');
   imageName = signal<string>('Ninguna imagen seleccionada');
   finalImageName = signal<string | null>(null);
   uploadImage = signal<boolean>(false);
   filePath = signal<string | null>(null);
   file = signal<any>(null);
   requestImageStatus = signal<RequestStatus>('init');
-  BUCKET_NAME = 'product_images';
 
+  /* Form */
   form = this.formBuilder.group({
     code: ['', [Validators.required]],
     name: ['', [Validators.required]],
@@ -51,22 +63,50 @@ export class ProductForm implements OnInit{
     stock: [0, [Validators.required, Validators.pattern(/^\d+$/), Validators.min(0)]],
   });
 
+  /* NgOnInit */
   ngOnInit(): void {
-    this.getCategories()
+    this.getCategories();
+    this.activateRoute.params.subscribe((params: Params) => {
+      const id = params['id'];
+      if (!id || isNaN(id)) return;
+      this.productId.set(parseInt(id));
+      this.getProduct();
+    });
   }
 
+  /* GetCategories */
   async getCategories() {
-    this.requestStatusCategories.set("loading")
-    const { error, data} = await this.categoryService.getAll()
-    if(error) {
-      this.requestStatusCategories.set("error")
+    this.requestStatusCategories.set('loading');
+    const { error, data } = await this.categoryService.getAll();
+    if (error) {
+      this.requestStatusCategories.set('error');
     }
-    if(data) {
-      this.requestStatusCategories.set("success")
-      this.categories.set(data)
+    if (data) {
+      this.requestStatusCategories.set('success');
+      this.categories.set(data);
     }
   }
 
+  async getProduct() {
+    this.requestStatusLoading.set('loading');
+    const { error, data } = await this.productService.get(this.productId() as Product['id']);
+    if (error) {
+      alert('Ocurrió un error al recuperar la información');
+      this.requestStatusLoading.set('error');
+      this.form.disable();
+    }
+    if (data) {
+      this.formMode.set('edit');
+      this.product.set(data);
+      this.form.patchValue({
+        ...data,
+        category: data.category.id,
+      });
+      this.requestStatusLoading.set('success');
+    }
+  }
+
+  /* VerifyImageFile */
   verifyFile(e: any) {
     const newFile = e.target.files[0];
     if (!newFile) return;
@@ -88,6 +128,7 @@ export class ProductForm implements OnInit{
     }
   }
 
+  /* Upload Image */
   async upload() {
     if (this.file && this.filePath) {
       this.requestImageStatus.set('loading');
@@ -109,6 +150,7 @@ export class ProductForm implements OnInit{
     }
   }
 
+  /* Save the form */
   async save() {
     this.form.markAllAsTouched();
     if (this.form.invalid) return;
@@ -117,8 +159,11 @@ export class ProductForm implements OnInit{
     if (this.uploadImage()) {
       await this.upload();
       if (this.requestImageStatus() === 'error') {
-        alert('Error subiendo la imagen');
+        this.errorMessage.set('Error al cargar la imagen');
         this.form.enable();
+        setTimeout(() => {
+          this.requestImageStatus.set('init');
+        }, 3000);
         return;
       }
       dto.image = this.finalImageName() as string;
@@ -129,6 +174,7 @@ export class ProductForm implements OnInit{
 
     if (error) {
       this.requestStatus.set('error');
+      this.errorMessage.set('Error al crear el producto');
       setTimeout(() => {
         this.requestStatus.set('init');
         this.form.enable();
