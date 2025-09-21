4079fa75185b108e296a4a852274d336dd06acdb
Alex Coronell <alexcoronell@gmail.com>
2025-09-21 15:47:37 -0500

    Product detail layout completed

--- src/app/products/features/product-shell/product.route.ts
+++ src/app/products/features/product-shell/product.route.ts
@@ -1,5 +1,12 @@
-import { Routes } from "@angular/router";
+import { Routes } from '@angular/router';
 
 export default [
-    { path: '', loadComponent: () => import('../product-list/product-list').then(m => m.ProductList) }
-] as Routes
+  {
+    path: '',
+    loadComponent: () => import('../product-list/product-list').then((c) => c.ProductList),
+  },
+  {
+    path: ':id',
+    loadComponent: () => import('../product-detail/product-detail').then((c) => c.ProductDetail),
+  },
+] as Routes;
